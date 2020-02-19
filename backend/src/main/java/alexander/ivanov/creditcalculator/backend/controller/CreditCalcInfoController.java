6c07314511368e74563086a0c4ad9e436dac1505
ivanovaa <IvanovAA@tdc.ru.com>
2020-02-20 09:57:00 +1000

    changed controllers request

--- backend/src/main/java/alexander/ivanov/creditcalculator/backend/controller/CreditCalcInfoController.java
+++ backend/src/main/java/alexander/ivanov/creditcalculator/backend/controller/CreditCalcInfoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/credit-calc-info")
+@RequestMapping("/api/credit-calc-infos")
 @RestController
 public class CreditCalcInfoController {
     private CreditCalcInfoService creditCalcInfoService;
--- backend/src/main/java/alexander/ivanov/creditcalculator/backend/controller/CreditController.java
+++ backend/src/main/java/alexander/ivanov/creditcalculator/backend/controller/CreditController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/credit")
+@RequestMapping("/api/credits")
 @RestController
 public class CreditController {
     private CreditService creditService;
